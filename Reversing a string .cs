711b2e47f27cb06348f9a1f87a4d1976570ea759
Alexander Boev <58472873+superalex-dev@users.noreply.github.com>
2021-04-13 10:03:28 +0300

    Add files via upload

---
+++ Reversing a string .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string reversed = new string(input.Reverse().ToArray());
+            Console.WriteLine(reversed);
+        }
+    }
+}
